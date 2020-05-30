5e56246432806584848e3779820c60a16d321365
smueksch <sebastian.mueksch+github@gmail.com>
2020-05-30 21:59:01 +0200

    Adding Simulation struct.

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,8 @@
-mod utils;
-
 use wasm_bindgen::prelude::*;
+use fixedbitset::FixedBitSet;
+use js_sys::Math;
+
+mod utils;
 
 // When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
 // allocator.
@@ -8,12 +10,45 @@ use wasm_bindgen::prelude::*;
 #[global_allocator]
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
+/*
+ * Constants
+ */
+
+const SPIN_UP: bool = true;
+const SPIN_DOWN: bool = false;
+
+const SPIN_DOWN_PROBABILITY: f64 = 0.3;
+
+/*
+ * Simulation
+ */
+
 #[wasm_bindgen]
-extern {
-    fn alert(s: &str);
+struct Simulation {
+    width: u32,
+    height: u32,
+    spins: FixedBitSet,
 }
 
 #[wasm_bindgen]
-pub fn greet() {
-    alert("Welcome to our 2D Ising Model Simulation 🖥️!");
+impl Simulation {
+    pub fn new(width: u32, height: u32) -> Simulation {
+        let size = (width * height) as usize;
+        let mut spins = FixedBitSet::with_capacity(size);
+
+        for i in 0..spins.len() {
+            spins.set(i, Math::random() >= SPIN_DOWN_PROBABILITY);
+        }
+        
+        Simulation {
+            width,
+            height,
+            spins,
+        }
+    }
+
+    /// Return pointer to the start of the spins collection.
+    pub fn spins(&self) -> *const u32 {
+        self.spins.as_slice().as_ptr()
+    }
 }
