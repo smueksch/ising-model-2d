942d2f75f14f23868c53ad8f323c9c572d442c56
smueksch <sebastian.mueksch+github@gmail.com>
2020-05-30 22:09:49 +0200

    Added simulation and plot size constants.

--- www/index.js
+++ www/index.js
@@ -1,14 +1,37 @@
 import * as wasm from "ising-model-2d";
 
+/*
+ * Constants
+ */
+
+const SIMULATION_WIDTH = 1000;
+const SIMULATION_HEIGHT = 1000;
+
+const PLOT_WIDTH = 400;
+const PLOT_HEIGHT = 200;
+
+const SPIN_WIDTH = 2; // px
+const SPIN_HEIGHT = 2; // px
+
+/*
+ * Initialize Control Panel
+ */
+
+document.getElementById("simulation-width").value = SIMULATION_WIDTH;
+document.getElementById("simulation-height").value = SIMULATION_HEIGHT;
+
+document.getElementById("plot-width").value = PLOT_WIDTH;
+document.getElementById("plot-height").value = PLOT_HEIGHT;
+
 const startStopButton = document.getElementById("start-stop-button");
 
 const canvas = document.getElementById("simulation-canvas");
-canvas.width = 200
-canvas.height = 200
+canvas.width = PLOT_WIDTH;
+canvas.height = PLOT_HEIGHT;
 
 const context = canvas.getContext("2d");
 context.fillStyle = "#000000";
-context.fillRect(0, 0, 200, 200);
+context.fillRect(0, 0, PLOT_WIDTH, PLOT_HEIGHT);
 
 let running = false;
 
